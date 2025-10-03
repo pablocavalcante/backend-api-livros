410ea97a2c2f50bea33694175ad0390d50f8ee24
pablocavalcante <pablocavalcante@prodam.sp.gov.br>
2025-10-03 12:16:50 -0300

    feat(livros): implementa ordenação customizada por status

--- src/services/ListLivrosService.ts
+++ src/services/ListLivrosService.ts
@@ -1,37 +1,9 @@
 import prismaClient from "../prisma";
 
-interface Livro{
-_id: { $oid: string };
-  titulo: string;
-  autor: string;
-  status: 'EmLeitura' | 'PretendoLer' | 'Lido';
-}
-
 class ListLivrosService {
     async execute(){
 
-        const pipeline = [
-          {
-            $addFields: {
-              ordem: {
-                $switch: {
-                  branches: [
-                    { case: { $eq: ['$status', 'EmLeitura'] }, then: 1 },
-                    { case: { $eq: ['$status', 'PretendoLer'] }, then: 2 },
-                    { case: { $eq: ['$status', 'Lido'] }, then: 3 },
-                  ],
-                  default: 99,
-                },
-              },
-            },
-          },
-          { $sort: { ordem: 1 } },
-          { $project: { ordem: 0 } },
-        ];
-
-        const livros = await prismaClient.livros.aggregateRaw({
-          pipeline: pipeline,
-        }) as unknown as Livro[]; // 2. Fazemos a asserção do tipo aqui
+        const livros = await prismaClient.livros.findMany()
 
         return livros;
 
