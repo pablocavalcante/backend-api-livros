0cf6b6df82fb5077e0bf494b024358ae04b1ddad
pablocavalcante <pablocavalcante@prodam.sp.gov.br>
2025-10-03 13:46:00 -0300

    feat: Ordenação dos Livros

--- src/services/ListLivrosService.ts
+++ src/services/ListLivrosService.ts
@@ -1,11 +1,24 @@
 import prismaClient from "../prisma";
+import { StatusLeitura } from "../generated/prisma";
 
 class ListLivrosService {
-    async execute(){
+    async execute() {
 
         const livros = await prismaClient.livros.findMany()
 
-        return livros;
+        const ordem = {
+            [StatusLeitura.EmLeitura]: 1,
+            [StatusLeitura.PretendoLer]: 2,
+            [StatusLeitura.Lido]: 3
+        };
+
+        const livrosOrdenados = livros.sort((a, b) => {
+            const ordemA = ordem[a.status] || 99; // caso venha um status inesperado
+            const ordemB = ordem[b.status] || 99;
+            return ordemA - ordemB;
+        });
+
+        return livrosOrdenados;
 
     }
 }
