d04925b4caf4bfc051414b21e061b7e36adb52fb
pablocavalcante <pablocavalcante@prodam.sp.gov.br>
2025-10-03 12:14:46 -0300

    feat(livros): implementa ordenação customizada por status

--- src/controllers/ListLivrosController.ts
+++ src/controllers/ListLivrosController.ts
@@ -4,11 +4,16 @@ import { ListLivrosService } from '../services/ListLivrosService';
 
 class ListLivrosController{
     async handle(request: FastifyRequest, reply: FastifyReply){
-        const listLivroService = new ListLivrosService();
-
-        const livros = await listLivroService.execute();
-
-        reply.send(livros);
+        try {
+            const listLivroService = new ListLivrosService();
+            const livros = await listLivroService.execute();
+            return reply.send(livros);
+        } catch (error) {
+            // Se o serviço lançar um erro, ele será capturado aqui
+            console.error("Erro no controller:", error);
+            // E nós enviamos uma resposta de erro para o cliente
+            return reply.status(500).send({ message: "Erro ao listar livros." });
+        }
     }
 }
 
--- src/services/ListLivrosService.ts
+++ src/services/ListLivrosService.ts
@@ -1,9 +1,37 @@
 import prismaClient from "../prisma";
 
+interface Livro{
+_id: { $oid: string };
+  titulo: string;
+  autor: string;
+  status: 'EmLeitura' | 'PretendoLer' | 'Lido';
+}
+
 class ListLivrosService {
     async execute(){
 
-        const livros = await prismaClient.livros.findMany()
+        const pipeline = [
+          {
+            $addFields: {
+              ordem: {
+                $switch: {
+                  branches: [
+                    { case: { $eq: ['$status', 'EmLeitura'] }, then: 1 },
+                    { case: { $eq: ['$status', 'PretendoLer'] }, then: 2 },
+                    { case: { $eq: ['$status', 'Lido'] }, then: 3 },
+                  ],
+                  default: 99,
+                },
+              },
+            },
+          },
+          { $sort: { ordem: 1 } },
+          { $project: { ordem: 0 } },
+        ];
+
+        const livros = await prismaClient.livros.aggregateRaw({
+          pipeline: pipeline,
+        }) as unknown as Livro[]; // 2. Fazemos a asserção do tipo aqui
 
         return livros;
 
